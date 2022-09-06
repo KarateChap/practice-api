3df900869ea24550802d91db089934c5447973bd
Ralph Gabriel Mariano <57673613+KarateChap@users.noreply.github.com>
2022-09-06 15:46:07 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const PopulateArray = (num1, num2, num3) => {
+    let arr = [];
+
+    for (let i = 0; i < num1; i++) {
+      arr[i] = [];
+      for (let j = 0; j < num2; j++) {
+        arr[i][j] = [];
+        for (let k = 0; k < num3; k++) {
+          arr[i][j][k] = i * j * k;
+        }
+      }
+    }
+
+    return arr;
+  };
+
+  const ShowMaxSentence = (fullText, maxWord) => {
+    const textArr = fullText.split(" ");
+    const newTextArr = [];
+    let checkPoint;
+
+    if (maxWord > textArr.length) {
+      maxWord = textArr.length;
+    }
+
+    for (let i = 0; i < maxWord; i++) {
+      if (
+        textArr[i].includes("?") ||
+        textArr[i].includes("!") ||
+        textArr[i].includes(".")
+      ) {
+        checkPoint = i;
+      }
+    }
+
+    for (let i = 0; i < checkPoint + 1; i++) {
+      newTextArr.push(textArr[i]);
+    }
+
+
+    const newText = newTextArr.join(" ");
+    return newText;
+  };
+
+  //Input values here
+
+  console.log(PopulateArray(3, 3, 3));
+
+
+  // console.log(
+  //   ShowMaxSentence(
+  //     "How much is that doggie in the window? I do hope that doggie's for sale.",
+  //     9
+  //   )
+  // );
+
+  return <div></div>;
 }
 
 export default App;
